046db1218e57f09f06f5cea914766733986ce458
mark <markkixkithkim@gmail.com>
2024-05-01 22:09:38 +0300

    code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const anchors = links.map( (navTools) => (
+    <a key={navTools} href={"#" + navTools}>{navTools}</a>
+  ))
+  return <nav>{/* display an <a> tag for each link here */}{anchors}</nav>
 }
 
 export default NavBar;
